865418621193489f62ab8d1fffab0ae16be2e751
rayyush <ayush070503@gmail.com>
2025-05-01 10:19:27 +0530

    cleaned data

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ def load_transactions(file):
     try:
         df = pd.read_csv(file)
         df.columns = [col.strip() for col in df.columns]
-        
+        df["Amount"] = df["Amount"].str.replace(",", "").astype(float)
+        df["Date"] = pd.to_datetime(df["Date"], format="%d %b %Y")
         st.write(df)
         return df
     except Exception as e:
